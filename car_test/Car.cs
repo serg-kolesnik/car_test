4ee166d1694e811dacc6482e57d53b38e58e0291
Сергей Колесник <serg.kolesnik08@gmail.com>
2026-04-13 10:41:31 +0300

    Добавьте файлы проекта.

---
+++ car_test/Car.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace car_test
+{
+    internal class Car
+    {
+        int id;
+        string mark;
+        string color;
+
+        public Car()
+        {
+            id = 0;
+            mark = "Волга";
+            color = "Черный";
+        }
+
+        public Car(int id, string mark, string color)
+        {
+            this.id = id;
+            this.mark = mark;
+            this.color = color;
+        }
+
+        public Car(int id, string mark)
+        {
+            this.id = id;
+            this.mark = mark;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Mark { get => mark; set => mark = value; }
+        public string Color
+        {
+            get { return color; }
+            set
+            {
+                if (value.ToLower() == "радужный") throw new Exception("Радужный цвет не поддерживается");
+                color = value;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{id} {mark}, {color}";
+        }
+
+        public static int checkPrice(Car car)
+        {
+            int price = 0;
+            if (new[] { "волга", "москвич", "жигули" }.Contains(car.Mark.ToLower()))
+            {
+                price += 100000;
+            }
+            else if (new[] { "мерседес", "бмв", "ауди" }.Contains(car.Mark.ToLower()))
+            {
+                price += 300000;
+            }
+            else if (new[] { "ламборгини", "феррари", "порше" }.Contains(car.Mark.ToLower()))
+            {
+                price += 1000000;
+            }
+            else
+            {
+                price += 50000;
+            }
+            return price;
+        }
+
+        public static bool testDrive(Car car)
+        {
+            bool success = true;
+            if (new[] { "бмв", "москвич", "жигули", "волга" }.Contains(car.Mark.ToLower()))
+            {
+                success = Convert.ToBoolean(new Random().Next(0, 5));
+            }
+            return success;
+        }
+    }
+}
---
+++ car_test/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace car_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Car newCar = new Car();
+            listBox1.Items.Add(newCar);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Car selectedCar = (Car)listBox1.SelectedItem;
+            if (listBox1.SelectedIndex != -1)
+            {
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+                textBox3.Enabled = true;
+                textBox1.Text = (selectedCar).Id.ToString();
+                textBox2.Text = (selectedCar).Mark;
+                textBox3.Text = (selectedCar).Color;
+            }
+            else
+            {
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Car selectedCar = (Car)listBox1.SelectedItem;
+            MessageBox.Show($"Цена машины: {Car.checkPrice(selectedCar)}.");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Car selectedCar = (Car)listBox1.SelectedItem;
+            if (Car.testDrive(selectedCar) == true)
+            {
+                MessageBox.Show("Тест-драйв прошёл успешно!");
+            }
+            else
+            {
+                MessageBox.Show("Тест-драйв не удался!");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int newId = Convert.ToInt32(textBox1.Text);
+            string newMark = textBox2.Text;
+            string newColor = textBox3.Text;
+            Car newCar = new Car(newId, newMark, newColor);
+            listBox1.Items[listBox1.SelectedIndex] = newCar;
+        }
+    }
+}
